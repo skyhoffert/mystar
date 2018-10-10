2a2f366a8ed9f1fea4b190bfe59a0762ad86c729
skyhoffert <skwylarh@yahoo.com>
2018-10-10 03:12:53 -0400

    Added simple routes to test different functionality.

--- server.py
+++ server.py
@@ -11,9 +11,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
     return 'Index.'
+
+@app.route('/stars', methods=['GET'])
+def stars():
+    return 'Stars.'
     
 if __name__ == '__main__':
     app.run()
