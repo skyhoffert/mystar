ad340f686b10f8647001c045ce2b1398ed66525b
skyhoffert <skwylarh@yahoo.com>
2018-10-10 03:09:51 -0400

    Simple server for now to test connectivity.

---
+++ server.py
@@ -0,0 +1,19 @@
+__author__ = 'Sky Hoffert'
+__copyright__ = 'Copyright 2018, Sky Hoffert'
+__credits__ = ['Sky Hoffert']
+__license__ = 'MIT'
+__version__ = '0.0.1'
+__maintainer__ = 'Sky Hoffert'
+__email__ = 'skyhoffert@gmail.com'
+__status__ = 'Development'
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Index.'
+    
+if __name__ == '__main__':
+    app.run()
