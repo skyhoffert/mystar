d5f7a6fad61d0b03ae8cdfc2d204e97fec8ff2ba
skyhoffert <skwylarh@yahoo.com>
2018-11-02 00:41:27 -0400

    Random and constants have been figured out, now starting generation with a simple name.

--- generate_system.js
+++ generate_system.js
@@ -1,14 +1,24 @@
 // Sky Hoffert
 // November 1, 2018
 
+var constants = require('./constants');
+
 /* CONSTANTS ***********************************************************************************************/
-NUM_SAMPLES = 100
-SEED_INIT = 1
+const NUM_SAMPLES = 100
+const SEED_INIT = 1
 
 /* GLOBAL VARIABLES ****************************************************************************************/
 var seed = SEED_INIT;
 
 /* FUNCTIONS ***********************************************************************************************/
+function generate_system_name(){
+    let system_name  = String(Math.floor(1000 + random(0, 9000)));
+    system_name += ' ' + String(constants.SYSTEM_NAMES[1][Math.floor(random(0, constants.SYSTEM_NAMES[1].length))]);
+    system_name += '-' + String(constants.SYSTEM_NAMES[1][Math.floor(random(0, constants.SYSTEM_NAMES[1].length))]);
+    system_name += ' ' + String(constants.SYSTEM_NAMES[0][Math.floor(random(0, constants.SYSTEM_NAMES[0].length))]);
+
+    return system_name;
+}
 // Standard Normal variate using Box-Muller transform.
 function random_bm(mean=0.5, sigma=0.125) {
     let u = 0, v = 0;
@@ -33,4 +43,21 @@ function random(min=0, max=1) {
     return x;
 }
 
+// DEBUG
+function DEBUG(msg){
+    console.log('DEBUG: ' + msg)
+}
+
 /* MAIN PROGRAM ********************************************************************************************/
+// Modify the seed, if given as a command line argument
+if (process.argv[2] != null){
+    const intval = parseInt(process.argv[2], 10)
+    if (intval){
+        seed = intval
+    }
+}
+
+// generate a system name!
+name = generate_system_name();
+
+console.log(name)
