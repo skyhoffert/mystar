6d4a4b41c384356b5271ec58bf359dfa378c6b17
skyhoffert <skwylarh@yahoo.com>
2018-11-02 01:03:13 -0400

    Added more functionality and modified the README.

--- README.md
+++ README.md
@@ -1,23 +1,50 @@
 # mystar
 Interactive program where users can create their own Star System!
 
-## How to Use This Software
+---
+
+## python3
+
+### How to Use This Software
 Version 0.1 consists of a simple python3 program that generates a random Star System given a seed. This will create a json object and save it to a file with the system name as its title. As long as you download the directory as is, it should have everything it needs to run. 
 
-#### Requirements
+##### Requirements
 You need to download some python3 libraries to run the software, including:
 * datetime
 * json
 * numpy
 
-#### Running It
+##### Running It
 To run, simple use the command
 
 *python3 generate_system.py <seed>*
 
 Where *<seed>* is replaced with an integer value. Some interesting seeds that my operating system (OS) generated are listed in the "Seeding Notes" section, below.
 
-## Seeding Notes
+### Seeding Notes
 The *generate_system.py* file can be seeded with a single command line integer argument. Some interesting seeds that I used on my OS are listed below if you would like to experiment with them! I cannot guarantee seeds will generate the same systems (my OS is 64-bit Windows 10).
 * 0 - "3732 Pi-Chi Andromeda B" A system with a good mix of planets and stars
 * 5 - "3915 Omicron-Pi Serpens A" Neutron star with 13 planets :O
+
+---
+
+## javascript
+
+### How to Use This Software
+Version 0.1 consists of a simple node.js program that generates a random Star System given a seed. This will create a json object and save it to a file with the system name as its title. As long as you download the directory as is, it should have everything it needs to run.
+
+##### Requirements
+You need to download some javascript dependencies to run the software, including:
+ * node.js
+
+##### Running It
+To run, simple use the command
+
+*node generate_system.js <seed>*
+
+Where *<seed>* is replaced with an integer value. Some interesting seeds are listed in the "Seeding Notes" section, below.
+**NOTE:** A *<seed>* of 0 will not differ from a seed of 1 for this version
+
+### Seeding Notes
+The *generate_system.js* file can be seeded with a single command line integer argument. Some interesting seeds are listed below if you would like to experiment with them! I cannot guarantee seeds will generate the same systems (my OS is 64-bit Windows 10), but given the random number generation, it should work!
+* 12 - "3437 Rho-Beta Scorpius" A system with 4 Stars.
--- generate_system.js
+++ generate_system.js
@@ -11,14 +11,6 @@ const SEED_INIT = 1
 var seed = SEED_INIT;
 
 /* FUNCTIONS ***********************************************************************************************/
-function generate_system_name(){
-    let system_name  = String(Math.floor(1000 + random(0, 9000)));
-    system_name += ' ' + String(constants.SYSTEM_NAMES[1][Math.floor(random(0, constants.SYSTEM_NAMES[1].length))]);
-    system_name += '-' + String(constants.SYSTEM_NAMES[1][Math.floor(random(0, constants.SYSTEM_NAMES[1].length))]);
-    system_name += ' ' + String(constants.SYSTEM_NAMES[0][Math.floor(random(0, constants.SYSTEM_NAMES[0].length))]);
-
-    return system_name;
-}
 // Standard Normal variate using Box-Muller transform.
 function random_bm(mean=0.5, sigma=0.125) {
     let u = 0, v = 0;
@@ -28,19 +20,60 @@ function random_bm(mean=0.5, sigma=0.125) {
     num = num / 10.0 + 0.5; // Translate to 0 -> 1
     let diff_mean = mean - 0.5;
     let diff_stddev = sigma / 0.125
-    num = num + diff_mean
-    num = num * diff_stddev
+    num += diff_mean
+    num *= diff_stddev
     return num
 }
-
 // Random uniform value between 0 and 1
 function random(min=0, max=1) {
     let x = Math.sin(seed++) * 10000;
     x = x - Math.floor(x);
     let range = max - min;
-    x = x * range;
-    x = x + min;
+    x *= range;
+    x += min;
     return x;
+}
+
+// actual specific generation functions ****
+
+/* 
+Generates a new, random system name!
+    @return: string; name of a new system
+*/
+function generate_system_name(){
+    let system_name  = String(Math.floor(1000 + random(0, 9000)));
+    system_name += ' ' + String(constants.SYSTEM_NAMES[1][Math.floor(random(0, constants.SYSTEM_NAMES[1].length))]);
+    system_name += '-' + String(constants.SYSTEM_NAMES[1][Math.floor(random(0, constants.SYSTEM_NAMES[1].length))]);
+    system_name += ' ' + String(constants.SYSTEM_NAMES[0][Math.floor(random(0, constants.SYSTEM_NAMES[0].length))]);
+
+    return system_name;
+}
+
+/* 
+Returns a value from a predefined list meeting the proper format
+    @arg list: list; holds values that match the format (value, weight)
+    @return: variable; the "random" value generated
+*/
+function generate_value_from_list(list){
+    // first, calculate the total value of all weights
+    var total_dist = 0;
+    var i = 0;
+    for (i = 0; i < list.length; i++){
+        total_dist += list[i][1];
+    }
+    
+    // generate a random value
+    var val = random(0,1) * total_dist;
+    var total_running = 0;
+    
+    // now, find which type based on that random value
+    for (i = 0; i < list.length; i++){
+        total_running += list[i][1];
+        if (val < total_running){
+            return list[i][0];
+        }
+    }
+    return list[0][0]
 }
 
 // DEBUG
@@ -58,6 +91,8 @@ if (process.argv[2] != null){
 }
 
 // generate a system name!
-name = generate_system_name();
+let system_name = generate_system_name();
+console.log(system_name)
 
-console.log(name)
+// generate number of Stars
+let num_stars = generate_value_from_list(constants.NUM_STARS);
