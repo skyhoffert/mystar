7d5ea3629505bef844e5c6f00479d63d2de0ab38
skyhoffert <skwylarh@yahoo.com>
2018-11-03 14:30:04 -0400

    Now simply drawing entire system, with name! Nearly ready for alpha version.

--- drawing.js
+++ drawing.js
@@ -1,13 +1,16 @@
 // Sky Hoffert
 // November 3, 2018
 
+// require the generation script
 const generate_system = require('./generate_system');
 
+// grab the canvas from the html document and set things for it
 var c = document.getElementById("main_canvas");
 c.width = 1280;
 c.height = 720;
 var ctx = c.getContext("2d");
 
+// keep track of the current system
 var system = null;
 
 // set frame rate to 30 fps
@@ -20,8 +23,15 @@ function update(){
 
     // draw stars near the center
     draw_stars();
+
+    // draw planets elsewhere
+    draw_planets();
 }
 
+/*
+Draw stars for the global system variable
+    @return: void
+*/
 function draw_stars(){
     if (system){
         if (system['stars'].length === 1){
@@ -45,6 +55,14 @@ function draw_stars(){
     }
 }
 
+/*
+Draw stars for the global system variable
+    @arg color: string; 6-digit hex value (Ex. #AB1234) or color mapping (Ex. red) representing star color
+    @arg x: int; center of star, x
+    @arg y: int; center of star, y
+    @arg r: int; radius of star, in pixels
+    @return: void
+*/
 function draw_star(color, x, y, r){
     ctx.beginPath();
     ctx.fillStyle = color;
@@ -53,6 +71,34 @@ function draw_star(color, x, y, r){
     ctx.closePath();
 }
 
+/*
+Draw planets for the global system variable
+    @return: void
+*/
+function draw_planets(){
+    for (let i = 0; i < system['planets'].length; i++){
+        ctx.beginPath();
+        ctx.fillStyle = system['planets'][i]['colors'][0];
+        ctx.arc(planet_x_by_i(i), c.height/2, 2, 0, 2*Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+/*
+Return a position for planet at given index. This function is temporary TODO
+    @return: int; position of planet on screen in pixels
+*/
+function planet_x_by_i(i){
+    // even numbers are on the right side
+    return i % 2 === 0 ? c.width/2 + ((i/2)*25 + 200) : c.width/2 - ((i/2)*25 + 200);
+}
+
+/*
+Calculate and return radius for a given type of star
+    @arg star: dict; describing the target star
+    @return: int; radius of star in pixels
+*/
 function radius_of(star){
     if (star['type'] == 'Brown Dwarf'){
         return 8;
@@ -73,6 +119,12 @@ function radius_of(star){
     }
 }
 
+/*
+Loads a new star system using the generate_system file into the global system variable
+    @return: void;
+*/
 function load_new_system(){
     system = generate_system.new_system();
+
+    document.getElementById('system_name').innerHTML = system['name'];
 }
