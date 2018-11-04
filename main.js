c2c765fefb6dce696b5662dff5e359f018a93c2d
skyhoffert <skwylarh@yahoo.com>
2018-11-04 01:38:48 -0400

    Restructured files slightly, ready for release to a website.

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.on('ready', function(){
 
     // load html into window
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
+        pathname: path.join(__dirname, 'mystar_main.html'),
         protocol: 'file:',
         slashes: true
     }));
